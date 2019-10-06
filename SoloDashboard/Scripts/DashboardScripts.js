dcf0447d2bbb4087d28c4b610a0474ef7c1f80f3
Philip <Philip@DESKTOP-4I5I4D0>
2019-10-05 23:02:30 -0400

    Created UI using bootstrap 4

---
+++ SoloDashboard/Scripts/DashboardScripts.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    //$('#loaderbody').addClass('hide');
+
+    //var Popup;
+
+    //ActiveDataTable();
+    //AjaxEventNotifications();
+
+
+    //$(document).on('change', '.status-dropdown', function () {
+    //    var selectedStatus = $(this).find('option:selected').text();
+    //    var Url = $(this).attr('data-url');
+    //    var jobNumber = $(this).attr('data-jobNumber');
+    //    var selectedDropdown = $(this).attr('data-name');
+
+    //    StatusChangeUpdate(selectedStatus, Url, jobNumber, selectedDropdown);
+
+    //});
+
+});
